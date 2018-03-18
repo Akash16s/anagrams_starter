1858a880eaddd8a0823b4bd839f71eac1df78144
Akash16s <akashs1698@gmail.com>
2018-03-18 17:25:41 +0530

    intermediate

--- app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
+++ app/src/main/java/com/google/engedu/anagrams/AnagramDictionary.java
@@ -20,6 +20,8 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -30,22 +32,38 @@ public class AnagramDictionary {
     private static final int MAX_WORD_LENGTH = 7;
     private Random random = new Random();
     private ArrayList<String> wordList;
-    private ArrayList<String> wordset;
+    private HashSet<String> wordSet;
+    private HashMap<String, ArrayList<String>> lettersToWord;
+
 
     public AnagramDictionary(Reader reader) throws IOException {
         BufferedReader in = new BufferedReader(reader);
         String line;
         wordList=new ArrayList<>();
+        wordSet = new HashSet<>();
+        lettersToWord = new HashMap<>();
+
         while((line = in.readLine()) != null) {
             String word = line.trim();
             wordList.add(word);
+            wordSet.add(word);
+            String add=sortLetters(word);
+            if(lettersToWord.containsKey(add)){
+                lettersToWord.get(add).add(word);
+            }else{
+                ArrayList<String> r= new ArrayList<>();
+                r.add(word);
+                lettersToWord.put(add,r);
+            }
+
+
 
 
         }
     }
 
     public boolean isGoodWord(String word, String base) {
-        return true;
+        return wordSet.contains(word) && !word.contains(base);
     }
 
     public List<String> getAnagrams(String targetWord) {
@@ -60,16 +78,25 @@ public class AnagramDictionary {
         return result;
     }
 
+    public ArrayList<String> getAnagramsWithOneMoreLetter(String word) {
+        ArrayList<String> temp = new ArrayList<>();
+        String k;
+        for(char i='1';i<='z';i++){
+            k = sortLetters(word.concat(""+i));
+            if (lettersToWord.containsKey(k)){
+                temp.addAll(lettersToWord.get(k));
+            }
+
+        }
+        return temp;
+    }
+
     public String sortLetters(String word) {
         char [] sorted= word.toCharArray();
         Arrays.sort(sorted);
         return new String (sorted);
     }
-
-    public List<String> getAnagramsWithOneMoreLetter(String word) {
-        ArrayList<String> result = new ArrayList<String>();
-        return result;
-    }
+    
 
     public String pickGoodStarterWord() {
         return "stop";
